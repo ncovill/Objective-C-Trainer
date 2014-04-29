c071453326edf9b73eb160424b484edc24514161
natec <ncovill@gmail.com>
2014-04-29 15:47:52 -0400

    Initial project commit
    Initial project commit

---
+++ ObjectiveTrainerApp/ObjectiveTrainerApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObjectiveTrainerApp
+//
+//  Created by Nathan Covill on 4/29/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjectiveTrainerApp/ObjectiveTrainerApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjectiveTrainerApp
+//
+//  Created by Nathan Covill on 4/29/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
